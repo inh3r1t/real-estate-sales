7b8978b8a51c9baacdea113fa63c5bd4c8e23b4a
Eraop <493214262@qq.com>
2018-11-30 22:31:25 +0800

    静态页面

---
+++ code/real-estate-sales-wx/pages/building/select/index.js
@@ -0,0 +1,156 @@
+// pages/building/select/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+      id: 1,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水，32层挑高超大户型，依山傍水',
+      commission: '1.5%+12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1.5%'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1.5%+12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1%'
+
+    }, {
+      id: 1,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水，32层挑高超大户型，依山傍水',
+      commission: '1.5%+12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1.5%'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1.5%+12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '12000'
+
+    }, {
+      id: 2,
+      thumb: '/images/house1.png',
+      housename: '滁州万科城市中心',
+      companyname: '万科',
+      houseaddress: '滁州万科城市中心',
+      summary: '32层挑高超大户型，依山傍水',
+      commission: '1%'
+
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
