9a773b3f49f2c2db35fc9978dab474528345f19a
Eraop <493214262@qq.com>
2018-12-16 22:38:57 +0800

    我的客户页面相关功能

--- code/real-estate-sales-wx/pages/customer/index/index.js
+++ code/real-estate-sales-wx/pages/customer/index/index.js
@@ -1,25 +1,29 @@
-// pages/customer/index.js
+var http = require('../../../utils/http.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    currentTab: 0,
     showShareModal: false,
-    shareData: "客户到访<br/>项目名称：山河印<br/>客户姓名：陈先生<br/>客户电话：‭130 *** 5158<br/>报备时间：2018.11.21<br/>到访时间：2018.11.21<br/>渠道公司：德信安居<br/>项目经理：13888888888"
+    shareData: "客户到访<br/>项目名称：山河印<br/>客户姓名：陈先生<br/>客户电话：‭130 *** 5158<br/>报备时间：2018.11.21<br/>到访时间：2018.11.21<br/>渠道公司：德信安居<br/>项目经理：13888888888",
+    page: 1,
+    list: [],
+    count: {
+      total_count: 0,
+      report_count: 0,
+      arrive_count: 0,
+      subscribe_count: 0,
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.getList(1);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -28,56 +32,59 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    this.getList(1, true).then(() => {
+      // 处理完成后，终止下拉刷新
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
+    this.getList(this.data.page + 1)
   },
+  //点击切换
+  clickTab: function(e) {
+    if (this.data.currentTab === e.currentTarget.dataset.current) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: e.currentTarget.dataset.current,
+        list: []
+      })
+      this.getList(1, true);
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+    }
   },
   toDetail: function() {
     wx.navigateTo({
       url: '/pages/customer/detail/detail',
     })
   },
-  confirm: function() {
-    wx.showModal({
-      title: '标题',
-      content: '这是一个模态弹窗',
-      success(res) {
-        if (res.confirm) {
-          console.log('确定')
-        } else if (res.cancel) {
-          console.log('取消')
-        }
-      }
+  operate: function(e) {
+    const index = e.currentTarget.dataset.index
+    const state = e.currentTarget.dataset.state
+    const id = e.currentTarget.dataset.id
+    const currentItem = this.data.list[index]
+    let url = "";
+    if (state == 0) {
+      // 报备用户 -> 预约
+      url = '/pages/customer/appointment/appointment?id=' + id + "&index=" + index
+    } else if (state == 1) {
+      // 预约用户 -> 到访
+      url = '/pages/customer/visit/visit?id=' + id + "&index=" + index
+    } else if (state == 2) {
+      // 报备到访 -> 认购
+      url = '/pages/customer/subscribe/subscribe?id=' + id + "&index=" + index
+    }
+    wx.navigateTo({
+      url: url,
     })
   },
   share: function() {
@@ -111,5 +118,36 @@ Page({
       showShareModal: false
     })
 
+  },
+  getList(pageNo, override) {
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+
+    var state = '';
+    if (this.data.currentTab == 1) {
+      state = 0;
+    }
+    if (this.data.currentTab == 2) {
+      state = 2;
+    }
+    if (this.data.currentTab == 3) {
+      state = 3;
+    }
+    return http.get("https://www.easy-mock.com/mock/5c0fa08f5324d050e6ab1ada/real-estate-sales/getCustomerFlows?state=" + state).then(res => {
+      //这里既可以获取模拟的res
+      console.log(res)
+      this.setData({
+        list: override ? res.data : this.data.list.concat(res.data),
+        count: res.count
+      })
+      console.log(this.data.list)
+      // 隐藏加载框
+      wx.hideLoading();
+    }).catch(err => {
+      console.log("==> [ERROR]", err)
+      // 隐藏加载框
+      wx.hideLoading();
+    })
   }
 })
