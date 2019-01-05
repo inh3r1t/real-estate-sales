638fd87fa816ba0d5c820a1af415525ef2fd3ee6
inh3rit <pyf3237829>
2019-01-05 17:57:16 +0800

    update

--- code/real-estate-sales-manager/src/main/java/com/zx/business/service/BusAgentCompanyService.java
+++ code/real-estate-sales-manager/src/main/java/com/zx/business/service/BusAgentCompanyService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * @Author: ytxu3
@@ -60,22 +62,17 @@ public class BusAgentCompanyService {
         return busAgentCompanyMapper.updateByPrimaryKeySelective(busRealEstate);
     }
 
-    private static String[] chars = new String[] { "a", "b", "c", "d", "e", "f",
-            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-            "W", "X", "Y", "Z" };
-
-    public static String generateShortUuid() {
-        StringBuffer shortBuffer = new StringBuffer();
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-        for (int i = 0; i < 8; i++) {
-            String str = uuid.substring(i * 4, i * 4 + 4);
-            int x = Integer.parseInt(str, 16);
-            shortBuffer.append(chars[x % 0x3E]);
-        }
-        return shortBuffer.toString();
+    public String generateShortUuid() {
+        List<BusAgentCompany> busAgentCompanies = busAgentCompanyMapper.selectByPage(null, null, null, null, new BusAgentCompany());
+        List<String> pollCodes = busAgentCompanies.stream()
+                .map(BusAgentCompany::getPollCode)
+                .collect(Collectors.toList());
+        Random random = new Random();
+        String pollCode;
+        do {
+            pollCode = String.valueOf(random.nextInt(10000));
+        } while (pollCodes.contains(pollCode));
+        return pollCode;
     }
 
 }
