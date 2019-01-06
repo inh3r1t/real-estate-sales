8978f62d08dc3aa4a0385d3d8ae27e5d239599a9
inh3rit <pyf3237829>
2019-01-06 17:37:37 +0800

    update

--- code/real-estate-sales-manager/src/test/java/com/zx/base/common/MsgTemplateTest.java
+++ code/real-estate-sales-manager/src/test/java/com/zx/base/common/MsgTemplateTest.java
@@ -30,7 +30,7 @@ public class MsgTemplateTest {
         params.put("type", "0");
         params.put("template_name", "公司内部通知模板");
         params.put("autograph", "安策系统通知");
-        params.put("content", "你好，有一条你的{1}通知，负责人电话：{2}。订单号：{3}");
+        params.put("content", "你好，有一条你的{1}通知，业务订单号（{2}），联系人：{3}  详情请查看安策营销系统！");
 
         HttpEntity httpEntity = HttpKit.post(url, null, JSON.toJSONString(params), header, Charset.defaultCharset());
         System.out.println(httpEntity.getHtml());
