f2fc1dcc2dc809c8a2323dc870a71b05ef2f0fd9
ytxu3 <>
2019-01-07 16:33:56 +0800

    update

---
+++ code/real-estate-sales-manager/src/main/resources/init/data_init.sql
@@ -0,0 +1,7 @@
+--bus_agent_company
+INSERT INTO `real-estate-sales`.bus_agent_company (id, name, poll_code, phone, address, state, charge_person, extend1, extend2, extend3, extend4) VALUES (1, '安策营销', '3337', '13955004532', '丰乐大道888号', 0, '陈浩', null, null, null, null);
+INSERT INTO `real-estate-sales`.bus_agent_company (id, name, poll_code, phone, address, state, charge_person, extend1, extend2, extend3, extend4) VALUES (2, '滁州市南谯区德信安居房产中介服务部', '5827', '13955004532', '滁州市南谯区草塘路87号', 1, '陈浩', null, null, null, null);
+
+--bus_role
+INSERT INTO `real-estate-sales`.bus_role (id, name, type, create_time, update_time) VALUES (1, '驻场经理', '0', '2018-12-25 20:44:13', '2018-12-25 20:44:13');
+INSERT INTO `real-estate-sales`.bus_role (id, name, type, create_time, update_time) VALUES (2, '中介', '1', '2018-12-25 22:42:07', '2018-12-25 22:42:07');
