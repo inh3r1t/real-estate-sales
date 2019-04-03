d35909e5ce44cadbad6cc1b9c843b88c3ad8eb82
eraop <493214262@qq.com>
2019-04-03 21:35:45 +0800

    添加楼盘分享功能及区域筛选

--- code/real-estate-sales-wx/pages/building/detail/detail.js
+++ code/real-estate-sales-wx/pages/building/detail/detail.js
@@ -1,4 +1,7 @@
 var WxParse = require('../../../wxParse/wxParse.js');
+const util = require('../../../utils/util.js')
+import Card from '../../../utils/image.js';
+import painter from "../../../components/painter/painter.js"
 var app = getApp()
 Page({
 
@@ -12,7 +15,17 @@ Page({
     duration: 1000,
     isLogin: false,
     isManager: false,
-    building: {}
+    isShare: false,
+    showShareModal: false,
+    building: {},
+
+    articleTitle: '',
+    articleShareDes: '',
+    hasPower: false,
+    hasAuth: false,
+    bgPic: '/img/bz_bg.png',
+    template: {},
+    shareImage: '',
   },
   makePhoneCall: function() {
     wx.makePhoneCall({
@@ -24,8 +37,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    debugger
+    var source = options.source;
+    console.log(options);
+
+    if (source == "share") {
+      this.setData({
+        isShare: true
+      })
+    }
     var id = options.id;
-    if (id != undefined && id > 0) {
+    const scene = decodeURIComponent(options.scene);
+    if (scene != undefined && scene != "undefined") {
+      id = scene;
+      this.setData({
+        isShare: true
+      })
+    }
+    if (id != undefined && id != '') {
       app.get("/busRealEstate/getById/" + id).then(res => {
         // console.log(res);
         this.setData({
@@ -65,6 +94,31 @@ Page({
         isManager: user.busRole.type == 0
       })
     }
+
+    wx.getSetting({
+      success: (res) => {
+        console.log(res.authSetting['scope.writePhotosAlbum'] === undefined)
+        if (res.authSetting['scope.writePhotosAlbum'] === undefined) {
+          this.setData({
+            hasAuth: false
+          })
+        } else {
+          this.setData({
+            hasAuth: true
+          })
+          if (res.authSetting['scope.writePhotosAlbum']) {
+            this.setData({
+              hasPower: true
+            })
+          } else {
+            this.setData({
+              hasPower: false
+            })
+          }
+          console.log(this.data.hasPower)
+        }
+      }
+    })
   },
   report: function() {
     app.checkLogin().then(res => {
@@ -79,5 +133,77 @@ Page({
       current: current, // 当前显示图片的http链接
       urls: this.data.building.images // 需要预览的图片http链接列表
     })
-  }
+  },
+  share: function() {
+    let userInfo = wx.getStorageSync('userInfo');
+    var articleTitle = "买房请联系 " + userInfo.phoneNum + "";
+    var articleShareDes = this.data.building.summery;
+    // var articleTitleUrl = util.getCurrentPageUrlWithArgs() + "&source=share";
+    var articleTitleUrl = this.data.building.images[0];
+    var miniAppImg = "/images/logo.png";
+    var miniCdoe = "";
+    var bgPic = "/images/bg.png";
+    app.get("/busRealEstate/gainMiniProgramCode?page=" + util.getCurrentPageUrl() + "&scene=" + this.data.building.id).then(res => {
+      miniCdoe = res.data;
+      console.log(res);
+      this.setData({
+        showShareModal: true,
+        template: new Card().palette(bgPic, articleTitleUrl, miniCdoe, miniAppImg, articleTitle, articleShareDes),
+      });
+    })
+
+  },
+  onShareAppMessage(res) {
+    if (res.from === 'button') {
+      this.setData({
+        showShareModal: true
+      });
+    } else {
+      let userInfo = wx.getStorageSync('userInfo');
+      return {
+        title: "买房请联系 " + userInfo.phoneNum,
+        path: util.getCurrentPageUrlWithArgs() + "&source=share",
+        // imageUrl: this.data.building.images[0]
+      }
+    }
+
+  },
+  hideModal: function() {
+    this.setData({
+      showShareModal: false
+    });
+  },
+  onImgOK(e) {
+    this.setData({
+      shareImage: e.detail.path
+    })
+  },
+  eventSave() {
+    let that = this;
+    wx.authorize({
+      scope: 'scope.writePhotosAlbum',
+      success() {
+        // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+        console.log(that.data.hasPower)
+        let imgSrc = that.data.shareImage;
+        wx.saveImageToPhotosAlbum({
+          filePath: imgSrc,
+          success: function(res) {
+            wx.showToast({
+              title: '图片已保存至本地相册',
+              icon: 'none'
+            })
+          },
+          fail: function() {
+            wx.showToast({
+              title: '保存失败',
+              icon: 'none'
+            })
+          }
+        })
+      }
+    })
+
+  },
+
 })
